27b6e59566a9769fee8be726309c1b82d3c18e9a
Alexandru-Razvan Olariu <ufcolonel@gmail.com>
2020-10-17 18:57:42 +0300

    Am refacut clasa colonelu

--- colonelu.java
+++ colonelu.java
@@ -1,8 +1,11 @@
-
-//import java.io.*;
+import java.io.*;
 
 public class colonelu{
     public static void main(String args[]){
     System.out.println("Colonelu e baros");
+	System.out.println("Moarte la francezi");
+	public static void println()
+	dsadas
+	;
     }
 }
